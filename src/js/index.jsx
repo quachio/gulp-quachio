914a2a4c1c652acf213a5481849152fbb4ac3546
Joel Longie <admin@joellongie.com>
2015-10-26 10:08:30 -0700

    updated index.jsx props

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import HelloWorld from './components/demoReact.jsx';
 
 ReactDOM.render(
-    <HelloWorld phrase="ES6"/>,
+    <HelloWorld phrase="React Component!"/>,
     document.querySelector('#reactApp')
 );
 
