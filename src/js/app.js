20594c8975bd611eb8fc2110af465b7a7ddc5b20
Joel Longie <admin@joellongie.com>
2016-03-29 23:04:17 -0700

    applied node coding standards

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,18 @@
-"use strict";
+'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactMessage from './components/Message';
 
 ReactDOM.render(
-    <ReactMessage phrase="React Component!"/>,
+    <ReactMessage phrase='React Component!'/>,
     document.querySelector('#app')
 );
 
 
-//es6
+// Es6
 import {es6Welcome} from './demoES6';
 es6Welcome(6);
 
 
 // https://github.com/ModuleLoader/es6-module-loader/wiki/Brief-Overview-of-ES6-Module-syntax
 console.log('javascript reporting in from app.js ...');
-
-
-
-
