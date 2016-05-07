ecf05eafe33848cc739b730f011b21964f2c58e2
Joel Longie <admin@joellongie.com>
2016-05-06 21:43:54 -0700

    added .jscsrc so suppress annoying rule about arrow functions

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(
-    <App phrase='ES6 React Class Component!'/>,
-    document.querySelector('#app')
+  <App phrase='ES6 React Class Component!'/>,
+  document.querySelector('#app')
 );
