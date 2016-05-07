166a64d50adaa25f6768bdbd6c9f34ad9e32c488
Joel Longie <admin@joellongie.com>
2016-05-06 21:52:16 -0700

    removed .jscsrc file, changed stateless react components arrow functions

--- src/js/components/StatelessComponentExample.js
+++ src/js/components/StatelessComponentExample.js
@@ -1,10 +1,8 @@
 'use strict';
 import React from 'react';
 
-const StatelessComponent = (props) => {
-  return (
+const StatelessComponent = (props) => (
   <p>I'm an example of a <strong>React Stateless Component</strong>, Yippee!</p>
-  );
-};
+);
 
 export default StatelessComponent;
