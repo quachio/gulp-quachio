605a18d83a382a6f021c4d8f95610a932a573268
Joel Longie <admin@joellongie.com>
2016-05-06 22:53:15 -0700

    removed componenets

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/app';
 
 ReactDOM.render(
   <App phrase='ES6 React Class Component!'/>,
