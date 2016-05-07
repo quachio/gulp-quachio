6f791cd05ae1dd6b665790d1bad37af07f05af95
Joel Longie <admin@joellongie.com>
2016-05-06 22:54:52 -0700

    renamed components to lower case

---
+++ src/js/components/statelessComponentExample.js
@@ -0,0 +1,8 @@
+'use strict';
+import React from 'react';
+
+const StatelessComponent = (props) => (
+  <p>I'm an example of a <strong>React Stateless Component</strong>, Yippee!</p>
+);
+
+export default StatelessComponent;
