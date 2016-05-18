168a40279c1e380977793ae060fb657dd4e43b33
Joel Longie <admin@joellongie.com>
2016-05-18 16:36:59 -0700

    reverted back to browserHistory as default

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// hashHistory (hash-based URLs, browserHistory (HTML5 pushState "pretty" URLs)
-import { Router, hashHistory } from 'react-router';
+// hashHistory (hash-based URLs), browserHistory (HTML5 pushState "pretty"
+// URLs). If problems with browserHistory use hashHistory.
+// Server must be configured to use HTML5 Pushstate.
+// With Apache server, create .htaccess using this gist:
+// https://gist.github.com/joellongie/e6f4a3b5fa98c521782619e487aca15a
+
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 ReactDOM.render(
-  <Router history={ hashHistory } routes={routes} />,
+  <Router history={ browserHistory } routes={routes} />,
   document.querySelector('#app')
 );
