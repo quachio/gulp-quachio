1d9299dff0f42a36f1f546a9fcb3372d127b9eac
Joel Longie <admin@joellongie.com>
2016-05-18 21:46:24 -0700

    update History message

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // ////////////////////////////////////////////////
-// hashHistory (hash-based URLs), browserHistory (HTML5 pushState "pretty"
-// URLs). If problems with browserHistory use hashHistory.
-// Server must be configured to use HTML5 Pushstate.
+// hashHistory (hash-based URLs), browserHistory (HTML5 pushState "pretty" URLs).
+// To use browserHistory properly server must be configured to use HTML5 Pushstate.
+// If problems with browserHistory use hashHistory.
 // With Apache server, create .htaccess using this gist:
 // https://gist.github.com/joellongie/e6f4a3b5fa98c521782619e487aca15a
-// // ////////////////////////////////////////////////
+// ////////////////////////////////////////////////
 
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
